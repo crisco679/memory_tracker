1bc228ef94e491fe2b5c89cbf13bc41b0130cc40
christuttle <crunch.tuttle@gmail.com>
2016-05-03 14:35:57 -0500

    Fixes delete button

--- server/routes/indexRoute.js
+++ server/routes/indexRoute.js
@@ -11,16 +11,16 @@ router.post('/store', function(request, response){
   Memory.save()
 });
 router.get('/memories', function(request, response){
-  return model.find({}).exec(function(err, memories){
+    model.find({}).exec(function(err, memories){
     if(err){
       console.log('Error', err);
     }
     response.send(JSON.stringify(memories));
   })
 })
-router.delete('/:id', function(request, response){
+router.delete('/memories/:id', function(request, response){
   console.log('Deleting requested profile id', request.params.id);
-  model.findOneAndRemove({_id: request.params.id}), function(err, memory){
+  model.findOneAndRemove({_id: request.params.id}, function(err, memory){
     if(err){
       console.log("Error removing profile from database w/ error", err);
       response.status(500).send(err);
@@ -28,7 +28,7 @@ router.delete('/:id', function(request, response){
       console.log("profile deleted", memory)
       response.sendStatus(200);
     }
-  }
+  });
 })
 router.get('/', function(request, response){
   response.sendFile(path.join(__dirname, '../public/views/index.html'))
